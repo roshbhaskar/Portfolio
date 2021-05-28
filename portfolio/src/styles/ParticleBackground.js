16155c69e5bd05a00e791d45d19d6ca4d94bc9c8
Roshini <roshbhaskar25@gmail.com>
2021-05-28 11:38:59 +0530

    sticker and background

---
+++ portfolio/src/styles/ParticleBackground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import particlesConfig from '../assets/config/particles-config';
+
+export default function ParticleBackground(){
+    return(
+        <Particles params={particlesConfig}/>
+    );
+}
