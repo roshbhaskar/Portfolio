7c332b6a98751d47c9b3f76ce6c7ab6dcd2e15b5
Roshini <roshbhaskar25@gmail.com>
2021-05-29 20:24:00 +0530

    Footer set for all pages

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 export default function Contact() {
     return (
         <div>
             <h1>Contact</h1>
+            <Footer/>
         </div>
     )
 }
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 export default function Projects() {
     return (
         <div>
             <h1>Project</h1>
+            <Footer/>
         </div>
     )
 }
